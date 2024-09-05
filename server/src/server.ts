2418d4519681eb8c4215cf713eb5d3c1dd1fc45a
Yuichi-Kadota <kadota.yuichi@gmail.com>
2024-09-06 02:44:45 +0900

    chore: error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { saveUser, fetchAllUsers, fetchUser } from "./repository/user";
 import { userSchema } from "./dto/user";
+import { BadRequestError } from "./lib/error";
 
 const app = new Hono();
 
@@ -13,14 +14,15 @@ app.get("/", (c) => {
 app.get("/user/:userId", async (c) => {
   const userId = c.req.param("userId");
   if (!userId) {
-    return c.json({ error: "", message: "missing userId" }, 400);
+    const error = new BadRequestError();
+    return c.json({ error }, error.status);
   }
 
   const result = await fetchUser(userId);
   console.log({ result });
   return result.match(
     (user) => c.json(user),
-    (error) => c.json({ error }, 404)
+    (error) => c.json({ error }, error.status)
   );
 });
 
@@ -28,7 +30,7 @@ app.get("/users", async (c) => {
   const result = await fetchAllUsers();
   return result.match(
     (users) => c.json(users),
-    (error) => c.json({ error }, 500)
+    (error) => c.json({ error }, error.status)
   );
 });
 
@@ -37,13 +39,14 @@ app.post("/user", async (c) => {
 
   const parseResult = userSchema.safeParse(body);
   if (!parseResult.success) {
-    return c.json({ error: parseResult.error.errors }, 400);
+    const error = new BadRequestError();
+    return c.json({ error }, error.status);
   }
 
   const insertRes = await saveUser(parseResult.data);
   return insertRes.match(
     (user) => c.json(user),
-    (error) => c.json({ error }, 500)
+    (error) => c.json({ error }, error.status)
   );
 });
 
